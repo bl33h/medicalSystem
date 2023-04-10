e0b39120e65922d6456dfaf17e0cfded4a6819a4
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-04-10 09:13:28 -0600

    Avances Expediente - Columns Names
    Falta que hagan el query para poder encontrar lo necesario, asi mismo falta la parte de poder editar el expediente, tambien se agrego una funcion para poder obtener el nombre de las columnas de los querys hechos asi como una funcion que permite el display automatico de cualquier valor retornado por un query.

--- resultadosExpediente.py
+++ resultadosExpediente.py
@@ -16,12 +16,26 @@ class ResultadoExpediente:
             listResults = list(results[i])
         
         for i in range(len(listResults)):
-            dicResults[listResults[i]] = Label(self.win, text=listResults[i])
+            dicResults[listResults[i]] = Entry(self.win)
+            dicResults[listResults[i]].insert(0, listResults[i]) # Inserta el valor en el Entry
+            
             
         if (len(listResults) == len(column_names)):
             for i in range(len(column_names)):
                 dicLabels[column_names[i]].pack()
                 dicResults[listResults[i]].pack()
+                
+        buttonModificar = Button(self.win, text="Modificar", command= lambda: ObtenerValoresNuevos())
+        buttonModificar.pack()
         
+        def ObtenerValoresNuevos():
+            newValores = []
+            for i in dicResults:
+                newValores.append(dicResults[i].get())
+            self.ModificaValores(newValores)
         
         self.win.geometry("600x600")
+        
+    def ModificaValores(self, newValores):
+        for i in newValores:
+            print(i)
