1ae8d64d0639abaa7f1b14a18f9275a1e67389f8
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-04-15 17:03:26 -0600

    funcion_azurdia_aplicada

--- resultadosExpediente.py
+++ resultadosExpediente.py
@@ -5,47 +5,15 @@ class ResultadoExpediente:
         self.parent = parent
         self.win = Toplevel(parent)
         self.win.title("Expediente")
-        dicLabels = {}
-        dicResults = {}
-        listResults = []
+        listColumnas = list(column_names)
         
-        for i in range(len(column_names)):
-            dicLabels[column_names[i]] = Label(self.win, text=column_names[i].capitalize())
-            
+        
         for i in range(len(results)):
-            listResults = list(results[i])
+            etiNoResultado = Label(self.win, text=f"Resultado {i+1}:", fg="#1e90ff")
+            etiNoResultado.pack()
+            texto = ""
+            for j in range(len(results[i])):
+                texto = texto + f"{listColumnas[j]}: {results[i][j]} "
+            etiResultado = Label(self.win, text=texto)
+            etiResultado.pack()    
         
-        keys = [] #Permite tener valores repetidos en el diccionario
-        for i in range(len(listResults)):
-            key = listResults[i]
-            while (key in dicResults):
-                key = key + "1"
-            dicResults[key] = Entry(self.win)
-            dicResults[key].insert(0, listResults[i]) # Inserta el valor en el Entry
-            keys.append(key)
-        
-        if (len(listResults) == len(column_names)):
-            for i in range(len(column_names)):
-                dicLabels[column_names[i]].pack()
-                dicResults[keys[i]].pack()
-            
-            
-        if (len(listResults) == len(column_names)):
-            for i in range(len(column_names)):
-                dicLabels[column_names[i]].pack()
-                dicResults[listResults[i]].pack()
-                
-        buttonModificar = Button(self.win, text="Modificar", command= lambda: ObtenerValoresNuevos())
-        buttonModificar.pack()
-        
-        def ObtenerValoresNuevos():
-            newValores = []
-            for i in dicResults:
-                newValores.append(dicResults[i].get())
-            self.ModificaValores(newValores)
-        
-        self.win.geometry("600x600")
-        
-    def ModificaValores(self, newValores):
-        for i in newValores:
-            print(i)
